be681ad7e33b81c3f0e1514efe4f09a421d7dbe8
Jayson Zhou <jaysonz6879@gmail.com>
2022-08-21 18:04:22 +0800

    [add] add ListNode and TreeNode classes.

---
+++ sharedClasses/TreeNode.java
@@ -0,0 +1,57 @@
+package sharedClasses;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    // a simple implementation for binary search tree node insertion
+    // Does not support null value input.
+    public void insertInBST(TreeNode node, int value) {
+        if (value < node.val) {
+            if (node.left != null) {
+                insertInBST(node.left, value);
+            } else {
+                node.left = new TreeNode(value);
+            }
+        } else if (value > node.val) {
+            if (node.right != null) {
+                insertInBST(node.right, value);
+            } else {
+                node.right = new TreeNode(value);
+            }
+        }
+    }
+
+    // a simple implementation for binary tree node insertion.
+    // Does not support null value input.
+    public void insertInBT(TreeNode node, int value) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        while (!q.isEmpty()) {
+            node = q.peek();
+            q.remove();
+            if (node.left == null) {
+                node.left = new TreeNode(value);
+                break;
+            } else {
+                q.add(node.left);
+            }
+            if (node.right == null) {
+                node.right = new TreeNode(value);
+                break;
+            } else {
+                q.add(node.right);
+            }
+        }
+    }
+}
