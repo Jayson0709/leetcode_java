41ff3bf62fd97dbd5657bd41fcb735fc93bb9a56
Jayson Zhou <jaysonz6879@gmail.com>
2022-08-22 08:37:28 +0800

    [update] add APIs for LinkedList input and BinaryTree input.

--- utils/InputMethods.java
+++ utils/InputMethods.java
@@ -1,6 +1,36 @@
 package utils;
-import java.util.*;
+
+import sharedClasses.ListNode;
+import sharedClasses.TreeNode;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class InputMethods {
+    public static ListNode getInputForListNode() {
+        Scanner cin = new Scanner(System.in, StandardCharsets.UTF_8);
+        int[] data = Arrays.stream(cin.nextLine().strip().split(" ")).mapToInt(Integer::parseInt).toArray();
+        cin.close();
+        ListNode head = new ListNode(data[0]);
+        ListNode tempHead = head;
+        for (int i = 1; i < data.length; i++) {
+            ListNode tempNode = new ListNode(data[i]);
+            tempHead.next = tempNode;
+            tempHead = tempNode;
+        }
+        return head;
+    }
+
+    public static TreeNode getInputForOneTree() {
+        Scanner cin = new Scanner(System.in, StandardCharsets.UTF_8);
+        int[] data = Arrays.stream(cin.nextLine().strip().split(" ")).mapToInt(Integer::parseInt).toArray();
+        cin.close();
+        TreeNode root = new TreeNode(data[0]);
+        for (int i = 1; i < data.length; i++) {
+            root.insertInBT(root, data[i]);
+        }
+        return root;
+    }
 }
