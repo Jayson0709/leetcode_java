cb559e4c20ab2fb785a6b6a2dc6613728f6f2f45
Jayson Zhou <jaysonz6879@gmail.com>
2022-08-22 10:23:38 +0800

    [add] add OneDArrayAndOneInt.java and OneDArrayAndTwoInt.java.

---
+++ utils/OneDArrayAndOneInt.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class OneDArrayAndOneInt {
+    public int[] array;
+    public int val;
+
+    public OneDArrayAndOneInt(int[] array, int val) {
+        this.array = array;
+        this.val = val;
+    }
+}
---
+++ utils/OneDArrayAndTwoInt.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class OneDArrayAndTwoInt {
+    public int[] array;
+    public int val1;
+    public int val2;
+
+    public OneDArrayAndTwoInt(int[] array, int val1, int val2) {
+        this.array = array;
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+}
