b6c043a4f0b59c13d3b34ac63c68f67634b42cbd
Jayson Zhou <jaysonz6879@gmail.com>
2022-08-22 10:30:14 +0800

    [add] add TwoOneDArray.java.

---
+++ utils/TwoOneDArray.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class TwoOneDArray {
+    public int[] array1;
+    public int[] array2;
+
+    public TwoOneDArray(int[] array1, int[] array2) {
+        this.array1 = array1;
+        this.array2 = array2;
+    }
+}
