2320a1c894187fa641f8aba6618e4bb7c1480bfe
Jayson Zhou <jaysonz6879@gmail.com>
2022-08-30 18:20:07 +0800

    [update] add description to Q168 and Q171.

--- algorithms/math/Q168.java
+++ algorithms/math/Q168.java
@@ -3,6 +3,71 @@ package algorithms.math;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+
+//Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+//
+//    For example:
+//
+//    A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28
+//    ...
+//
+//    Example 1:
+//    Input: columnNumber = 1
+//    Output: "A"
+//
+//    Example 2:
+//    Input: columnNumber = 28
+//    Output: "AB"
+//
+//    Example 3:
+//    Input: columnNumber = 701
+//    Output: "ZY"
+//
+//    Constraints:
+//    1 <= columnNumber <= 2^31 - 1
+
+
+
+//给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+//
+//    例如：
+//
+//    A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28
+//    ...
+//
+//    示例 1：
+//    输入：columnNumber = 1
+//    输出："A"
+//
+//    示例 2：
+//    输入：columnNumber = 28
+//    输出："AB"
+//
+//    示例 3：
+//    输入：columnNumber = 701
+//    输出："ZY"
+//
+//    示例 4：
+//    输入：columnNumber = 2147483647
+//    输出："FXSHRXW"
+//
+//    提示：
+//    1 <= columnNumber <= 2^31 - 1
+
+
+
 public class Q168 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in, StandardCharsets.UTF_8);
--- algorithms/math/Q171.java
+++ algorithms/math/Q171.java
@@ -3,6 +3,71 @@ package algorithms.math;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+
+//Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+//
+//    For example:
+//
+//    A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28
+//    ...
+//    
+//    Example 1:
+//    Input: columnTitle = "A"
+//    Output: 1
+//    
+//    Example 2:
+//    Input: columnTitle = "AB"
+//    Output: 28
+//    
+//    Example 3:
+//    Input: columnTitle = "ZY"
+//    Output: 701
+//
+//    Constraints:
+//    1 <= columnTitle.length <= 7
+//    columnTitle consists only of uppercase English letters.
+//    columnTitle is in the range ["A", "FXSHRXW"].
+
+
+
+//给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回 该列名称对应的列序号 。
+//
+//    例如：
+//
+//    A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28
+//    ...
+//
+//    示例 1:
+//    输入: columnTitle = "A"
+//    输出: 1
+//
+//    示例 2:
+//    输入: columnTitle = "AB"
+//    输出: 28
+//
+//    示例 3:
+//    输入: columnTitle = "ZY"
+//    输出: 701
+//
+//    提示：
+//    1 <= columnTitle.length <= 7
+//    columnTitle 仅由大写英文组成
+//    columnTitle 在范围 ["A", "FXSHRXW"] 内
+
+
+
 public class Q171 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in, StandardCharsets.UTF_8);
