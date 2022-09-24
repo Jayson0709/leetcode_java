5b321d5abd814b6ce299f7950604c07c31b90891
Jayson Zhou <jaysonz6879@gmail.com>
2022-09-24 23:49:15 +0800

    [update] add another solution to Q1652.

--- dataStructures/array/prefixSum/Q1652.java
+++ dataStructures/array/prefixSum/Q1652.java
@@ -86,6 +86,7 @@ public class Q1652 {
         System.out.println(Arrays.toString(decrypt(obj.array, obj.val)));
     }
 
+    // Method 1: prefix sum
     private static int[] decrypt(int[] code, int k) {
         int n = code.length;
         if (k == 0) {
@@ -119,4 +120,31 @@ public class Q1652 {
         }
         return code;
     }
+
+    // Method 2: Window Sliding
+//    private static int[] decrypt(int[] code, int k) {
+//        int n = code.length;
+//        if (k == 0) {
+//            return new int[n];
+//        }
+//        int[] res = new int[n];
+//        int[] newCode = new int[n * 2];
+//        System.arraycopy(code, 0, newCode, 0, n);
+//        System.arraycopy(code, 0, newCode, n, n);
+//        code = newCode;
+//        int l = k > 0 ? 1 : n + k;
+//        int r = k > 0 ? k : n - 1;
+//        int w = 0;
+//        for (int i = l; i <= r; i++) {
+//            w += code[i];
+//        }
+//        for (int i = 0; i < n; i++) {
+//            res[i] = w;
+//            w -= code[l];
+//            w += code[r + 1];
+//            l++;
+//            r++;
+//        }
+//        return res;
+//    }
 }
