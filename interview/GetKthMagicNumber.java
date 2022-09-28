4f3a7491f1656468e135675ca035c687d0ae319d
Jayson Zhou <jaysonz6879@gmail.com>
2022-09-28 21:59:31 +0800

    [add] add solution to question GetKthMagicNumber.

---
+++ interview/GetKthMagicNumber.java
@@ -0,0 +1,48 @@
+package interview;
+
+
+//Design an algorithm to find the kth number such that the only prime factors are 3, 5, and 7. Note that 3, 5, and 7 do not have to be factors, but it should not have any other prime factors. For example, the first several multiples would be (in order) 1, 3, 5, 7, 9, 15, 21.
+//
+//    Example 1:
+//    Input: k = 5
+//    Output: 9
+
+
+//有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+//
+//    示例 1:
+//    输入: k = 5
+//    输出: 9
+
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class GetKthMagicNumber {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in, StandardCharsets.UTF_8);
+        int k = cin.nextInt();
+        cin.close();
+        System.out.println(getKthMagicNumber(k));
+    }
+
+    private static int getKthMagicNumber(int k) {
+        int[] dp = new int[k + 1];
+        dp[1] = 1;
+        int p3 = 1, p5 = 1, p7 = 1;
+        for (int i = 2; i <= k; i++) {
+            int num3 = dp[p3] * 3, num5 = dp[p5] * 5, num7 = dp[p7] * 7;
+            dp[i] = Math.min(Math.min(num3, num5), num7);
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+            if (dp[i] == num7) {
+                p7++;
+            }
+        }
+        return dp[k];
+    }
+}
