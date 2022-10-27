c00fc7eee59e2d5976d3efef8070699e6a2c19cd
JaysonZhou <jaysonz6879@gmail.com>
2022-10-27 23:31:14 +0800

    [add] add another solution to Q1822.

--- algorithms/bitManipulation/Q1822.java
+++ algorithms/bitManipulation/Q1822.java
@@ -93,4 +93,18 @@ public class Q1822 {
         }
         return res > 0 ? 1 : -1;
     }
+
+    // Method 3: simple array
+//    private static int arraySign(int[] nums) {
+//        int sign = 1;
+//        for (int num : nums) {
+//            if (num == 0) {
+//                return 0;
+//            }
+//            if (num < 0) {
+//                sign = -sign;
+//            }
+//        }
+//        return sign;
+//    }
 }
