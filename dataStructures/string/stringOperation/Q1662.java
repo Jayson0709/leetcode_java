834fa806947fc70c6de0161d57bce3bf527c4471
JaysonZhou <jaysonz6879@gmail.com>
2022-11-01 08:42:04 +0800

    [update] add other solutions to Q1662.

--- dataStructures/string/stringOperation/Q1662.java
+++ dataStructures/string/stringOperation/Q1662.java
@@ -67,6 +67,7 @@ public class Q1662 {
         System.out.println(arrayStringsAreEqual(word1, word2));
     }
 
+    // Version 1
     private static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         StringBuilder strBuilder1 = new StringBuilder();
         StringBuilder strBuilder2 = new StringBuilder();
@@ -78,4 +79,38 @@ public class Q1662 {
         }
         return strBuilder1.toString().equals(strBuilder2.toString());
     }
+
+    // Version 2
+//    private static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+//        return join(word1).equals(join(word2));
+//    }
+//
+//    public static String join(String[] words) {
+//        StringBuilder ret = new StringBuilder();
+//        for (String s : words) {
+//            ret.append(s);
+//        }
+//        return ret.toString();
+//    }
+
+    // Version 3 iterate
+//    private static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+//        int p1 = 0, p2 = 0, i = 0, j = 0;
+//        while (p1 < word1.length && p2 < word2.length) {
+//            if (word1[p1].charAt(i) != word2[p2].charAt(j)) {
+//                return false;
+//            }
+//            i++;
+//            if (i == word1[p1].length()) {
+//                p1++;
+//                i = 0;
+//            }
+//            j++;
+//            if (j == word2[p2].length()) {
+//                p2++;
+//                j = 0;
+//            }
+//        }
+//        return p1 == word1.length && p2 == word2.length;
+//    }
 }
