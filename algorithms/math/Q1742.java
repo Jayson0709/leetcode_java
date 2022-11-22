a998e957cef86a74d904ef277fb83b4afa900f5d
JaysonZhou <jaysonz6879@gmail.com>
2022-11-23 00:26:30 +0800

    [add] add a new solution to Q1742.

--- algorithms/math/Q1742.java
+++ algorithms/math/Q1742.java
@@ -118,4 +118,20 @@ public class Q1742 {
         }
         return endZeroCount;
     }
+
+    // Version 2: use HashMap
+//    private static int countBalls(int lowLimit, int highLimit) {
+//        Map<Integer, Integer> count = new HashMap<>();
+//        int res = 0;
+//        for (int i = lowLimit; i <= highLimit; i++) {
+//            int box = 0, x = i;
+//            while (x != 0) {
+//                box += x % 10;
+//                x /= 10;
+//            }
+//            count.put(box, count.getOrDefault(box, 0) + 1);
+//            res = Math.max(res, count.get(box));
+//        }
+//        return res;
+//    }
 }
