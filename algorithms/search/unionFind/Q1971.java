ae65c2f8bee57c7c986ed09ee02573c8a3b7190e
JaysonZhou <jaysonz6879@gmail.com>
2022-12-26 21:54:53 +0800

    [add] add two more solutions to Q1971.

--- algorithms/search/unionFind/Q1971.java
+++ algorithms/search/unionFind/Q1971.java
@@ -79,6 +79,7 @@ public class Q1971 {
         System.out.println(result);
     }
 
+    // Method 1: Union Find
     private static boolean validPath(int n, int[][] edges, int source, int destination) {
         root = new int[n];
         for (int i = 0; i < n; i++) {
@@ -106,4 +107,62 @@ public class Q1971 {
             return root[n];
         }
     }
+
+    // Method 2: BFS
+//    private static boolean validPath(int n, int[][] edges, int source, int destination) {
+//        List<Integer>[] adj = new List[n];
+//        for (int i = 0; i < n; i++) {
+//            adj[i] = new ArrayList<>();
+//        }
+//        for (int[] edge : edges) {
+//            int x = edge[0], y = edge[1];
+//            adj[x].add(y);
+//            adj[y].add(x);
+//        }
+//        boolean[] visited = new boolean[n];
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        queue.offer(source);
+//        visited[source] = true;
+//        while (!queue.isEmpty()) {
+//            int vertex = queue.poll();
+//            if (vertex == destination) {
+//                break;
+//            }
+//            for (int next : adj[vertex]) {
+//                if (!visited[next]) {
+//                    queue.offer(next);
+//                    visited[next] = true;
+//                }
+//            }
+//        }
+//        return visited[destination];
+//    }
+
+    // Method 3: DFS
+//    private static boolean validPath(int n, int[][] edges, int source, int destination) {
+//        List<Integer>[] adj = new List[n];
+//        for (int i = 0; i < n; i++) {
+//            adj[i] = new ArrayList<>();
+//        }
+//        for (int[] edge : edges) {
+//            int x = edge[0], y = edge[1];
+//            adj[x].add(y);
+//            adj[y].add(x);
+//        }
+//        boolean[] visited = new boolean[n];
+//        return dfs(source, destination, adj, visited);
+//    }
+//
+//    public static boolean dfs(int source, int destination, List<Integer>[] adj, boolean[] visited) {
+//        if (source == destination) {
+//            return true;
+//        }
+//        visited[source] = true;
+//        for (int next : adj[source]) {
+//            if (!visited[next] && dfs(next, destination, adj, visited)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
