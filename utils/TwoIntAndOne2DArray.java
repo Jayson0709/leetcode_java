36d975833c2dc9e473b1c3248eac996757be492b
JaysonZhou <jaysonz6879@gmail.com>
2023-08-20 16:04:16 +0800

    [add] add class TwoIntAndOne2DArray.

---
+++ utils/TwoIntAndOne2DArray.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class TwoIntAndOne2DArray {
+    public int val1;
+    public int val2;
+    public int[][] array;
+
+    public TwoIntAndOne2DArray(int val1, int val2, int[][] array) {
+        this.val1 = val1;
+        this.val2 = val2;
+        this.array = array;
+    }
+}
